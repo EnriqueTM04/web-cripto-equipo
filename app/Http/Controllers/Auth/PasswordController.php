da8c7cebb43a4e05e2feabdfee74c914419bb415
Enrique TM04 <enriquetabo03@gmail.com>
2025-05-18 18:00:04 -0600

    fix: Update password validation and redirect logic in PasswordController; clean up title configuration in layouts

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -38,24 +38,29 @@ class PasswordController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $validated = $request->validate([
-            'current_password' => ['required'],
             'password' => ['required', 'min:8'],
             'password_confirmation' => ['required', 'same:password'],
         ]);
 
-        // verificar si la contraseña actual es correcta en la base de datos
-        if ($validated['current_password'] !== $request->user()->password) {
-            dd($validated['current_password'], $request->user()->password);
+        // verificar que las contraseñas coincidan
+        if ($validated['password'] !== $validated['password_confirmation']) {
             return back()->withErrors([
-                'current_password' => 'La contraseña actual es incorrecta.',
+                'password' => 'Las contraseñas no coinciden.',
             ]);
         }
+
+        // buscar contraseña encriptada en la base de datos
+        $user = \App\Models\User::where('email', $request->email)->first();
+        // regenerar el token de sesión
+        auth()->login($user);
+        $request->session()->regenerate();
 
         // actualizar la contraseña
         $request->user()->update([
             'password' => $validated['password'],
         ]);
 
-        return back()->with('status', 'password-updated');
+        // redireccionar al usuario a la página de inicio
+        return redirect()->route('dashboard');
     }
 }
