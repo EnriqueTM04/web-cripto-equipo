7b7682f0fe971df0bdb2fa02b843dabe9cb8c6e0
Enrique TM04 <enriquetabo03@gmail.com>
2025-05-19 19:30:22 -0600

    fix: Refactor password hashing logic to ensure validation occurs before hashing

--- resources/js/password.js
+++ resources/js/password.js
@@ -2,16 +2,15 @@
     if (document.querySelector(".form-sha")) {
         document.querySelector('.form-sha').addEventListener('submit', function(e) {
             var passInput = document.getElementById('password');
-            var hash = CryptoJS.SHA256(passInput.value).toString();
-            passInput.value = hash;
-
             var pwd = passInput.value;
             var error = validarPassword(pwd);
             if (error) {
                 e.preventDefault();
                 mostrarMensajeError(error);  
                 return false;
-            }          
+            }     
+            var hash = CryptoJS.SHA256(passInput.value).toString();
+            passInput.value = hash;     
 
             var passInput2 = document.getElementById('password_confirmation');
             var hash2 = CryptoJS.SHA256(passInput2.value).toString();
@@ -27,9 +26,6 @@
             passInput.value = hash;
 
             var passInput2 = document.getElementById('update_password_password');
-            var hash2 = CryptoJS.SHA256(passInput2.value).toString();
-            passInput2.value = hash2;
-
             var pwd = passInput2.value;
             var error = validarPassword(pwd);
             if (error) {
@@ -37,7 +33,9 @@
                 mostrarMensajeError(error);  
                 return false;
             }
-            
+            var hash2 = CryptoJS.SHA256(passInput2.value).toString();
+            passInput2.value = hash2;
+
             var errorMessage = document.getElementById('error-message');
             errorMessage.innerHTML = "";
             errorMessage.style.display = 'none';
@@ -51,32 +49,32 @@
 
     function mostrarMensajeError(mensaje) {
         // mostrar el mensaje de error despues del formulario
-            var errorMessage = document.createElement('div');
-            errorMessage.innerHTML = mensaje;
-            errorMessage.id = 'error-message';
-            errorMessage.className = 'alert alert-danger';
-            errorMessage.innerHTML = "";
-            errorMessage.style.display = 'none';
-            errorMessage.style.marginTop = '10px';
-            errorMessage.style.marginBottom = '10px';
-            errorMessage.style.textAlign = 'center';
-            errorMessage.style.fontSize = '14px';
-            errorMessage.style.fontWeight = 'bold';
-            errorMessage.style.color = '#721c24';
-            errorMessage.style.backgroundColor = '#f8d7da';
-            errorMessage.style.borderColor = '#f5c6cb';
-            errorMessage.style.padding = '10px';
-            errorMessage.style.borderRadius = '5px';
+        var errorMessage = document.createElement('div');
+        errorMessage.innerHTML = mensaje;
+        errorMessage.id = 'error-message';
+        errorMessage.className = 'alert alert-danger';
+        errorMessage.innerHTML = "";
+        errorMessage.style.display = 'none';
+        errorMessage.style.marginTop = '10px';
+        errorMessage.style.marginBottom = '10px';
+        errorMessage.style.textAlign = 'center';
+        errorMessage.style.fontSize = '14px';
+        errorMessage.style.fontWeight = 'bold';
+        errorMessage.style.color = '#721c24';
+        errorMessage.style.backgroundColor = '#f8d7da';
+        errorMessage.style.borderColor = '#f5c6cb';
+        errorMessage.style.padding = '10px';
+        errorMessage.style.borderRadius = '5px';
     }
 
 
     function validarPassword(pwd) {
-    if (pwd.length < 8) return "La contraseña debe tener al menos 8 caracteres.";
-    if (!/[A-Z]/.test(pwd)) return "Debe contener al menos una letra mayúscula.";
-    if (!/[a-z]/.test(pwd)) return "Debe contener al menos una letra minúscula.";
-    if (!/[0-9]/.test(pwd)) return "Debe contener al menos un número.";
-    if (!/[^a-zA-Z0-9]/.test(pwd)) return "Debe contener al menos un símbolo.";
-    if (/(.)\1\1\1/.test(pwd)) return "No puede haber más de 3 caracteres iguales consecutivos.";
-    return null;
-}
+        if (pwd.length < 8) return "La contraseña debe tener al menos 8 caracteres.";
+        if (!/[A-Z]/.test(pwd)) return "Debe contener al menos una letra mayúscula.";
+        if (!/[a-z]/.test(pwd)) return "Debe contener al menos una letra minúscula.";
+        if (!/[0-9]/.test(pwd)) return "Debe contener al menos un número.";
+        if (!/[^a-zA-Z0-9]/.test(pwd)) return "Debe contener al menos un símbolo.";
+        if (/(.)\1\1\1/.test(pwd)) return "No puede haber más de 3 caracteres iguales consecutivos.";
+        return null;
+    }
 })();
